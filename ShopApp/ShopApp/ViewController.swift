60c4455454cf2bb7914598e5f1b1329700d64054
anvarsaidov <anvar5737251saidov@gmail.com>
2023-07-27 21:58:07 +0300

    загрузка

---
+++ ShopApp/ShopApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShopApp
+//
+//  Created by AnvarSaidov on 27.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
